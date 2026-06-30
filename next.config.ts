3df4ef1c9dd985ce3b1bca3d3d816a8d6177f544
Mortadha Ben Said <mortadhabensaid28@gmail.com>
2026-06-30 16:16:17 +0200

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["images.unsplash.com"],
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
