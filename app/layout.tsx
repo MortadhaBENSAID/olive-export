f401ce4b1ce7f8db01bd4d6a1f5d6f973c976729
Mortadha Ben Said <mortadhabensaid28@gmail.com>
2026-06-30 13:33:18 +0200

    Fix TypeScript layout children type

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,11 @@ export const metadata: Metadata = {
   ],
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body style={{ margin: 0, fontFamily: "Arial" }}>
